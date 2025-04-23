376ced42a351deba3c0988f6902476f6f9988e85
Youngpuffy <levimuturi2@gmail.com>
2025-04-23 22:36:05 +0300

    adding navbar

--- E-commerce/src/component/navbar.jsx
+++ E-commerce/src/component/navbar.jsx
@@ -1,18 +1,32 @@
-import { Link } from 'react-router-dom';
-import { FaShoppingCart } from 'react-icons/fa';
+// 
+import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
 import { useCart } from '../context/CartContext';
 
 const Navbar = () => {
-  const { cartItems } = useCart();
-  
+  const { cartItems, toggleCart } = useCart();
+  const itemCount = cartItems.length;
+
   return (
-    <nav className="navbar">
-      <Link to="/" className="logo">E-Shop</Link>
-      <Link to="/cart" className="cart-link">
-        <FaShoppingCart />
-        <span className="cart-count">{cartItems.length}</span>
-      </Link>
+    <nav className="bg-blue-600 text-white p-4">
+      <div className="container flex justify-between items-center">
+        <h1>WOOLWORTHS.</h1>
+        <div>
+          <button 
+            className="text-white text-xl relative cart-button"
+            onClick={toggleCart}
+          >
+            <FiShoppingCart />
+            {itemCount > 0 && (
+              <span className="cart-count">
+                {itemCount}
+              </span>
+            )}
+          </button>
+        </div>
+      </div>
     </nav>
   );
 };
+
 export default Navbar;
