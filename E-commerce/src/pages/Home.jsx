15ed6cd00b3ebd7f6a5f0bedd4373c67b75a277d
Youngpuffy <levimuturi2@gmail.com>
2025-04-23 23:38:05 +0300

    home.jsx

---
+++ E-commerce/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/styles.css'
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1 className="">WELCOME TO THE HOUSE OF THE BEST</h1>
+    </div>
+  );
+};
+
+export default Home;
